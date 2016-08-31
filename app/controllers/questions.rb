bd7dad2aa75c76a124dd51359bf2c299e1c3db85
Matthew Behrens & Caitlin Johnson <pair+MatthewBehrens.caitlinlikesrobots@devbootcamp.com>
2016-08-31 18:34:02 -0500

    final commit end of wednesday

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -1,19 +1,15 @@
 delete '/categories/:category_id/questions/:question_id' do
   @question = Question.find(params[:question_id])
   @question.destroy
-  p "I destroyed"
-
   redirect "/categories/#{params[:category_id]}"
 end
 
 get '/categories/:id' do
   @category = Category.find(params[:id])
-  erb :'categories/questions'
+  erb :'categories/show'
 end
 
 post '/categories/:id' do
-  p "!!!!!!!!!!!!!!!!!!!!!!!!"
-  p params
   @question = Question.create(title: params[:title], content: params[:content], user_id: session[:user_id], category_id: params[:id])
   redirect '/'
 end
