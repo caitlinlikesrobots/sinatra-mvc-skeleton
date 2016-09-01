6c920efca629572e57cf27c7168f3b46a279824c
Matthew Behrens & Caitlin Johnson <pair+MatthewBehrens.caitlinlikesrobots@devbootcamp.com>
2016-09-01 14:15:12 -0500

    one vote per user

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,7 +1,13 @@
 post '/categories/:category_id/questions/:question_id' do
   @user = User.find(session[:user_id])
   @answer = Answer.find((params[:answer].to_i))
+  @votable_id = Vote.find_by(votable_id: @answer.id)
+
+  if @user.votes.include?(@votable_id)
+  else
   @vote = Vote.create(value: 1, votable: @answer, user_id: @user.id)
+  end
+
   if request.xhr?
     content_type :json
     { answer_id: @answer.id, points: @answer.total_votes}.to_json
