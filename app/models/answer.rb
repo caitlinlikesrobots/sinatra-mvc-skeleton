9c94c4d239038290308ba0efa879a83ad72a0e69
Matthew Behrens & Caitlin Johnson <pair+MatthewBehrens.caitlinlikesrobots@devbootcamp.com>
2016-09-01 13:22:30 -0500

    validates answer submissions

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,9 +1,15 @@
 require_relative 'vote_count_module'
 class Answer < ActiveRecord::Base
+  include VoteCount
+
   belongs_to :question
   belongs_to :user
   has_many :comments
   has_many :votes, as: :votable
 
-  include VoteCount
+  validates :content, presence: true
+  validates :content, length: {minimum: 2}
+  validates :content, length: {maximum: 750}
+
+
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,8 @@ class Question < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
   has_many :answers
+
+  validates :title, :content, presence: true
+  validates :title, length: {minimum: 2}, length: {maximum: 50}
+  validates :content, length: {minimum: 2}, length: {maximum: 750}
 end
